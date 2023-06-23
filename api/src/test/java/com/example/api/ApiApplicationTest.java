1ffb740245939092df711903f143dc337445cb01
qfs <2488569367@qq.com>
2023-06-23 12:47:07 +0800

    修正

---
+++ api/src/test/java/com/example/api/ApiApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example.api;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiApplicationTest {
+
+    @Autowired
+    public static void main() {
+    }
+
+}
---
+++ api/src/test/java/com/example/api/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.example.api.repository;
+
+import static org.junit.Assert.*;
+
+public class EmployeeRepositoryTest {
+
+}
